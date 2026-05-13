56c1e2af6cb389630967731e100f7bbf91195187
turbancreationss-coder <turbancreationss@gmail.com>
2026-05-13 15:37:17 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,20 +1,71 @@
-import { initializeApp } from 
+import { initializeApp }
+
+from
+
 "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-import { getFirestore } from 
+
+import {
+
+  getFirestore
+
+}
+
+from
+
 "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
+
+import {
+
+  getAuth
+
+}
+
+from
+
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyAhB1dHC1cZEhDEWElqh3VKv0SjrziUwWc",
-  authDomain: "turrbancreations.firebaseapp.com",
-  projectId: "turrbancreations",
-  storageBucket: "turrbancreations.firebasestorage.app",
-  messagingSenderId: "161027179043",
-  appId: "1:161027179043:web:974b0c91f5eeec4d7da796"
+
+  apiKey:
+  "AIzaSyAhB1dHC1cZEhDEWElqh3VKv0SjrziUwWc",
+
+  authDomain:
+  "turrbancreations.firebaseapp.com",
+
+  projectId:
+  "turrbancreations",
+
+  storageBucket:
+  "turrbancreations.firebasestorage.app",
+
+  messagingSenderId:
+  "161027179043",
+
+  appId:
+  "1:161027179043:web:974b0c91f5eeec4d7da796"
+
 };
 
-const app = initializeApp(firebaseConfig);
 
-const db = getFirestore(app);
+const app =
+initializeApp(firebaseConfig);
 
-export { db };
+
+const db =
+getFirestore(app);
+
+
+const auth =
+getAuth(app);
+
+
+export {
+
+  db,
+  auth
+
+};
